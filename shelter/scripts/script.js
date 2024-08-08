e652aa9fbc7a3f7859d3732adeccfc9bb8c6932f
Denis Nedelko <dnsndlk@gmail.com>
2024-08-08 16:16:30 +0300

    feat: add functional for buger-menu

--- shelter/scripts/script.js
+++ shelter/scripts/script.js
@@ -3,9 +3,24 @@ const navMenu = document.querySelector('nav');
 const body = document.querySelector('body');
 const linksNavMenu = document.querySelectorAll('.header-content__nav-link');
 const shadow = document.querySelector('.shadow');
+const headerContent = document.querySelector('.header-content');
 
 let isActive = true;
-const checkStateBody = (isActive) => {
+const checkPage = () => {
+	if (window.innerWidth < 768) {
+		if (window.document.title.toLowerCase() === 'shelter-pets') {
+			navMenu.classList.add('white-menu');
+		} else {
+			navMenu.classList.add('dark-menu');
+			console.log('add');
+		}
+	} else {
+		navMenu.classList.remove('white-menu');
+		navMenu.classList.remove('dark-menu');
+	}
+};
+
+const checkStateBody = isActive => {
 	if (isActive) {
 		body.classList.toggle('no-scroll');
 		shadow.classList.toggle('show');
@@ -17,16 +32,14 @@ const checkStateBody = (isActive) => {
 
 const toggleNavMenu = () => {
 	checkStateBody(isActive);
+	checkPage();
 	burgerBtn.classList.toggle('active-burger-btn');
 	navMenu.classList.toggle('show-nav-menu');
 };
 
-const transitionToLink = () => {
-	checkStateBody(!isActive);
-	closeNavMenu();
-};
 
 const closeNavMenu = () => {
+    checkStateBody(!isActive);
 	burgerBtn.classList.remove('active-burger-btn');
 	navMenu.classList.remove('show-nav-menu');
 };
@@ -37,10 +50,12 @@ const checkWindowSize = () => {
 		burgerBtn.classList.add('show-burger-btn');
 	} else {
 		burgerBtn.classList.remove('show-burger-btn');
+        checkPage();
 	}
 };
 
 burgerBtn.addEventListener('click', toggleNavMenu);
-linksNavMenu.forEach(link => link.addEventListener('click', transitionToLink));
+shadow.addEventListener("click", closeNavMenu)
+linksNavMenu.forEach(link => link.addEventListener('click', closeNavMenu));
 window.addEventListener('load', checkWindowSize);
 window.addEventListener('resize', checkWindowSize);
