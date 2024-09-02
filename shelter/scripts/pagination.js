3df6eaac3c4d4ca20cbfdc6209a6782ad29653b0
Denis Nedelko <dnsndlk@gmail.com>
2024-09-02 17:11:08 +0300

    commit

--- shelter/scripts/pagination.js
+++ shelter/scripts/pagination.js
@@ -144,7 +144,7 @@ const checkWindowSize = () => {
 	} else newChunkLength = 8;
 
     if (newChunkLength !== chunkLength) {
-
+        slideIndex
         pagesCounter = Math.ceil((slideIndex + 1) / newChunkLength);
         slideIndex = pagesCounter * newChunkLength - newChunkLength;
    
