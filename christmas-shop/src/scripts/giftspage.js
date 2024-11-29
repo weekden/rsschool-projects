a72936a23a10d7838d24a464b1e8a89ab9ee0d4d
DenisNedelko <dnsndlk@gmail.com>
2024-11-29 16:51:00 +0300

    fix: fixed pathname in gifts and home pages 2024-11-29 16:51:00

--- christmas-shop/src/scripts/giftspage.js
+++ christmas-shop/src/scripts/giftspage.js
@@ -2,39 +2,38 @@ import { getGifts, getRandomArray } from './data.js';
 import { appendCardInGiftsContainer } from './card.js';
 import { renderPopup } from './popup.js';
 
-const gifts = await getGifts('.././');
-const routeGiftsImg = `../src/img/`;
-
 if (window.location.pathname.includes('gifts.html')) {
-const pageGiftsContainer = document.querySelector('.page-gifts__container');
-const tabListContainer = document.querySelector('.page-gifts__tabs');
-tabListContainer.firstElementChild.classList.add('active-tab');
+	const gifts = await getGifts('.././');
+	const routeGiftsImg = `../src/img/`;
+	const pageGiftsContainer = document.querySelector('.page-gifts__container');
+	const tabListContainer = document.querySelector('.page-gifts__tabs');
+	tabListContainer.firstElementChild.classList.add('active-tab');
 
-appendCardInGiftsContainer(getRandomArray(gifts), pageGiftsContainer, routeGiftsImg);
+	appendCardInGiftsContainer(getRandomArray(gifts), pageGiftsContainer, routeGiftsImg);
 
-const filterCardCategory = (_arr, _category) => {
-	return _arr.filter(item => item.category === _category);
-};
+	const filterCardCategory = (_arr, _category) => {
+		return _arr.filter(item => item.category === _category);
+	};
 
-tabListContainer.addEventListener('click', event => {
-	const tabList = document.querySelectorAll('.page-gifts__tabs-tab');
-	const clickedTab = event.target.closest('.page-gifts__tabs-tab');
+	tabListContainer.addEventListener('click', event => {
+		const tabList = document.querySelectorAll('.page-gifts__tabs-tab');
+		const clickedTab = event.target.closest('.page-gifts__tabs-tab');
 
-	tabList.forEach(item => {
-		item.classList.remove('active-tab');
+		tabList.forEach(item => {
+			item.classList.remove('active-tab');
+		});
+		clickedTab.classList.add('active-tab');
+
+		if (clickedTab.id === 'all') {
+			appendCardInGiftsContainer(getRandomArray(gifts), pageGiftsContainer, routeGiftsImg);
+		} else appendCardInGiftsContainer(filterCardCategory(gifts, clickedTab.id), pageGiftsContainer, routeGiftsImg);
 	});
-	clickedTab.classList.add('active-tab');
 
-	if (clickedTab.id === 'all') {
-		appendCardInGiftsContainer(getRandomArray(gifts), pageGiftsContainer, routeGiftsImg);
-	} else appendCardInGiftsContainer(filterCardCategory(gifts, clickedTab.id), pageGiftsContainer, routeGiftsImg);
-});
-
-pageGiftsContainer.addEventListener('click', event => {
-	const isPopup = true;
-	const clickedCard = event.target.closest('.gift__card');
-	const cardIndex = +clickedCard.getAttribute('data-index-card');
-	const findIndex = gifts.findIndex(item => item.atribute === cardIndex);
-	renderPopup(gifts[findIndex], routeGiftsImg, isPopup);
-});
+	pageGiftsContainer.addEventListener('click', event => {
+		const isPopup = true;
+		const clickedCard = event.target.closest('.gift__card');
+		const cardIndex = +clickedCard.getAttribute('data-index-card');
+		const findIndex = gifts.findIndex(item => item.atribute === cardIndex);
+		renderPopup(gifts[findIndex], routeGiftsImg, isPopup);
+	});
 }
--- christmas-shop/src/scripts/homepage.js
+++ christmas-shop/src/scripts/homepage.js
@@ -2,17 +2,18 @@ import { getGifts, getRandomArray } from './data.js';
 import { appendCardInGiftsContainer } from './card.js';
 import { renderPopup } from './popup.js';
 
-const gifts = await getGifts('./');
-const giftsContainer = document.querySelector('.gifts__container');
-const routeHomePage = `src/img/`;
 if (window.location.pathname.includes('index.html')) {
-appendCardInGiftsContainer(getRandomArray(gifts).slice(0, 4), giftsContainer, routeHomePage);
+	const gifts = await getGifts('./');
+	const giftsContainer = document.querySelector('.gifts__container');
+	const routeHomePage = `src/img/`;
 
-giftsContainer.addEventListener('click', event => {
-	const isPopup = true;
-	const clickedCard = event.target.closest('.gift__card');
-	const cardIndex = +clickedCard.getAttribute('data-index-card');
-	const findIndex = gifts.findIndex(item => item.atribute === cardIndex);
-	renderPopup(gifts[findIndex], routeHomePage, isPopup);
-});
+	appendCardInGiftsContainer(getRandomArray(gifts).slice(0, 4), giftsContainer, routeHomePage);
+
+	giftsContainer.addEventListener('click', event => {
+		const isPopup = true;
+		const clickedCard = event.target.closest('.gift__card');
+		const cardIndex = +clickedCard.getAttribute('data-index-card');
+		const findIndex = gifts.findIndex(item => item.atribute === cardIndex);
+		renderPopup(gifts[findIndex], routeHomePage, isPopup);
+	});
 }
