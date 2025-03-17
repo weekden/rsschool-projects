d0a5d29a986c1f965e4745a1beb8aed36b94caaa
DenisNedelko <dnsndlk@gmail.com>
2025-03-17 13:04:46 +0300

    fix: fixed randomizer colors and refactor Wheel 2025-03-17 13:04:46

--- decision-making-tool/src/components/Wheel.ts
+++ decision-making-tool/src/components/Wheel.ts
@@ -6,18 +6,22 @@ export class Wheel {
   private readonly wheelContainer: HTMLElement;
   private readonly canvas: HTMLCanvasElement;
   private readonly context: CanvasRenderingContext2D | null;
+  private readonly renderList: Todo[];
+  private startAngle: number = -Math.PI / 2;
+  private segmentColors: string[] = [];
 
   constructor() {
+    this.renderList = LSControl.getListForRender();
     this.canvas = document.createElement('canvas');
     this.canvas.className = 'decision-wheel';
     this.canvas.width = 500;
     this.canvas.height = 500;
 
     this.context = this.canvas.getContext('2d');
-
     this.wheelContainer = createElement({ tag: 'div', classes: ['decision-wheel__wrapper'] });
     this.wheelContainer.append(this.canvas);
 
+    this.generateInitialColors(this.renderList);
     this.drawWheel();
   }
 
@@ -25,11 +29,9 @@ export class Wheel {
     return this.wheelContainer;
   }
 
-  public runAnimation(): void {}
-
-  private getSegmentsArray(listArray: Todo[]): Todo[] {
-    const totalWeight = listArray.reduce((acc, item) => acc + +item.weight, 0);
-    return listArray
+  private getSegmentsArray(renderList: Todo[]): Todo[] {
+    const totalWeight = renderList.reduce((acc, item) => acc + +item.weight, 0);
+    return renderList
       .map((item): Todo => {
         return {
           id: item.id,
@@ -40,14 +42,24 @@ export class Wheel {
       .sort(() => Math.random() - 0.5);
   }
 
-  private generateColor(): string {
+  private generateArryColor(length: number): string[] {
+    const collorArray: string[] = [];
     const letters = '0123456789ABCDEF';
     const colorLength = 6;
-    let color = '#';
-    for (let i = 0; i < colorLength; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+
+    for (let i = 0; i < length; i++) {
+      let color = '#';
+      for (let j = 0; j < colorLength; j++) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+      }
+      collorArray.push(color);
     }
-    return color;
+
+    return collorArray;
+  }
+
+  private generateInitialColors(renderList: Todo[]): void {
+    this.segmentColors = this.generateArryColor(renderList.length);
   }
 
   private getSlicedString(string: string): string {
@@ -56,7 +68,10 @@ export class Wheel {
   }
 
   private drawWheel(): void {
-    if (!this.context) return;
+    if (!this.context) {
+      return;
+    }
+
     const radius = (this.canvas.width / 2) * 0.95;
     const centerX = this.canvas.width / 2;
     const centerY = this.canvas.height / 2;
@@ -64,32 +79,30 @@ export class Wheel {
     const pointerHeight = radius * 0.2;
     const pointerWidth = radius * 0.15;
     const fontSize = radius * 0.075;
-
-    const items = LSControl.getListForRender();
-    const angles = this.getSegmentsArray(items);
-    let startAngle = -Math.PI / 2;
+    const segments = this.getSegmentsArray(this.renderList);
 
     this.canvas.width = this.canvas.width;
 
     // Segments
-    angles.forEach((item) => {
+    segments.forEach((item, index) => {
       if (!this.context) return;
-      const endAngle = startAngle + +item.weight;
-      const textAngle = (startAngle + endAngle) / 2;
+      const endAngle = this.startAngle + +item.weight;
+      const textAngle = (this.startAngle + endAngle) / 2;
       const textX = centerX + radius * 0.9 * Math.cos(textAngle);
       const textY = centerY + radius * 0.9 * Math.sin(textAngle);
-      const segmentHeight = (radius * (endAngle - startAngle)) / Math.PI;
+      const segmentHeight = (radius * (endAngle - this.startAngle)) / Math.PI;
 
       this.context.beginPath();
       this.context.moveTo(centerX, centerY);
-      this.context.arc(centerX, centerY, radius, startAngle, endAngle);
-      this.context.fillStyle = `${this.generateColor()}`;
+      this.context.arc(centerX, centerY, radius, this.startAngle, endAngle);
+      this.context.fillStyle = this.segmentColors[index];
       this.context.fill();
       this.context.lineWidth = 3;
       this.context.strokeStyle = '#ffffff';
       this.context.stroke();
       this.context.closePath();
 
+      // SegmentTextContent
       this.context.save();
       this.context.beginPath();
       this.context.translate(textX, textY);
@@ -107,7 +120,7 @@ export class Wheel {
       this.context.closePath();
       this.context.restore();
 
-      startAngle = endAngle;
+      this.startAngle = endAngle;
     });
 
     // Center
