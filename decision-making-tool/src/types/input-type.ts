1a7459514cfe3845d5b093e73b7dac1bab173588
DenisNedelko <dnsndlk@gmail.com>
2025-04-13 17:47:34 +0300

    refactor: refactor createTodo.ts 2025-04-13 17:47:34

---
+++ decision-making-tool/src/types/input-type.ts
@@ -0,0 +1,9 @@
+export type Input = {
+  type: string;
+  placeholder?: string;
+  value?: string;
+  list?: string;
+  id?: string;
+  classes: string[];
+  disabled?: boolean;
+};
--- decision-making-tool/src/utils/dom/createTodoLi.ts
+++ decision-making-tool/src/utils/dom/createTodoLi.ts
@@ -1,3 +1,4 @@
+import { createInputElement } from './createInput';
 import type { Todo } from '../../types/todo-type';
 
 export function createLi(
@@ -14,23 +15,8 @@ export function createLi(
   idSpan.classList.add('todo-item__id');
   idSpan.textContent = `${todo.id}`;
 
-  const titleInput = document.createElement('input');
-  titleInput.classList.add('todo-item__input', 'todo-item__input-title');
-  titleInput.type = 'text';
-  titleInput.value = todo.title;
-  titleInput.placeholder = 'Title';
-  titleInput.addEventListener('input', () => {
-    changeTitleCallback(todo.id, titleInput.value);
-  });
-
-  const weightInput = document.createElement('input');
-  weightInput.classList.add('todo-item__input', 'todo-item__input-weight');
-  weightInput.type = 'number';
-  weightInput.value = `${todo.weight}`;
-  weightInput.placeholder = 'Weight';
-  weightInput.addEventListener('input', () => {
-    changeWeightCallback(todo.id, weightInput.value);
-  });
+  const titleInput = createTitleInput(todo, changeTitleCallback);
+  const weightInput = createWeightInput(todo, changeWeightCallback);
 
   const deleteButton = document.createElement('button');
   deleteButton.classList.add('todo-item__button');
@@ -42,4 +28,26 @@ export function createLi(
 
   li.append(idSpan, titleInput, weightInput, deleteButton);
   return li;
+}
+
+function createTitleInput(todo: Todo, onChange: (id: string, value: string) => void): HTMLInputElement {
+  const input = createInputElement({
+    type: 'text',
+    value: todo.title,
+    placeholder: 'Title',
+    classes: ['todo-item__input', 'todo-item__input-title'],
+  });
+  input.addEventListener('input', () => onChange(todo.id, input.value));
+  return input;
+}
+
+function createWeightInput(todo: Todo, onChange: (id: string, value: string) => void): HTMLInputElement {
+  const input = createInputElement({
+    type: 'number',
+    value: `${todo.weight}`,
+    placeholder: 'Weight',
+    classes: ['todo-item__input', 'todo-item__input-weight'],
+  });
+  input.addEventListener('input', () => onChange(todo.id, input.value));
+  return input;
 }
