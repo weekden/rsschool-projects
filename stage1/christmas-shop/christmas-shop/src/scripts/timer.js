a7c7d76bf0a178478a8e5a85c1bcc6474ca59aa4
DenisNedelko <dnsndlk@gmail.com>
2025-11-30 15:20:38 +0300

    fix: update timer for every year 2025-11-30 15:20:38

--- stage1/christmas-shop/christmas-shop/src/scripts/timer.js
+++ stage1/christmas-shop/christmas-shop/src/scripts/timer.js
@@ -1,17 +1,25 @@
 const NEWYEAR = 1735689600;
 const secondsInDay = 24 * 60 * 60;
 const secondsInHour = 60 * 60;
-const secondsInMinut = 60;
+const secondsInMinute = 60;
+
+const getNewYearTimestamp = () => {
+  const now = new Date();
+  const year = now.getFullYear() + 1; 
+  const newYear = new Date(year, 0, 1, 0, 0, 0); 
+  return Math.floor(newYear.getTime() / 1000);
+};
 
 const getTime = () => {
 	const data = new Date();
-	const now = Math.floor(data.getTime() / 1000);
-	const secondsAfterNewYear = NEWYEAR - now;
+  const now = Math.floor(data.getTime() / 1000);
+  const NEWYEAR = getNewYearTimestamp();
+  const secondsAfterNewYear = NEWYEAR - now;
 
 	const days = Math.floor(secondsAfterNewYear / secondsInDay);
 	const hours = Math.floor((secondsAfterNewYear % secondsInDay) / secondsInHour);
-	const minutes = Math.floor((secondsAfterNewYear % secondsInHour) / secondsInMinut);
-	const seconds = secondsAfterNewYear % secondsInMinut;
+	const minutes = Math.floor((secondsAfterNewYear % secondsInHour) / secondsInMinute);
+	const seconds = secondsAfterNewYear % secondsInMinute;
 	return [days, hours, minutes, seconds];
 };
 
